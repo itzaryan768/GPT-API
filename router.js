77a0de4d221a9e5602cbc82a1da7ca6a367ee7a5
Itz Aryan <aryanchauhan78577@gmail.com>
2024-06-29 09:28:51 +0530

    Create router.js

---
+++ router.js
@@ -0,0 +1,9 @@
+// router.js
+const express = require('express');
+const gptHandler = require('./gptHandler');
+
+const router = express.Router();
+
+router.get('/gpt', gptHandler);
+
+module.exports = router;
